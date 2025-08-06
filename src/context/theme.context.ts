43927a4c0a464bb0bf1112cf5990303cde485403
codewithsaidul <codewithsaidul@gmail.com>
2025-08-06 22:57:09 +0600

    theme toggle & fonts added

---
+++ src/context/theme.context.ts
@@ -0,0 +1,17 @@
+import type { Theme } from "@/types/theme.types"
+import { createContext } from "react"
+
+
+
+
+type ThemeProviderState = {
+  theme: Theme
+  setTheme: (theme: Theme) => void
+}
+
+const initialState: ThemeProviderState = {
+  theme: "system",
+  setTheme: () => null,
+}
+
+export const ThemeProviderContext = createContext<ThemeProviderState>(initialState)
