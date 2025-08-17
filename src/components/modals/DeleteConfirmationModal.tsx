930c0dd774300af38d22b58d0026cc9354756889
codewithsaidul <codewithsaidul@gmail.com>
2025-08-17 12:32:23 +0600

    Tour type remove functionality added

---
+++ src/components/modals/DeleteConfirmationModal.tsx
@@ -0,0 +1,45 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import type { ReactNode } from "react"
+
+
+interface IDeleteProps {
+  children: ReactNode;
+  onConfirm: () => void;
+}
+
+export function DeleteConfirmationModal( { children, onConfirm }: IDeleteProps ) {
+
+  const handleDelete = () => {
+    onConfirm();
+    console.log("Hi Im confirm")
+  }
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={handleDelete}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
