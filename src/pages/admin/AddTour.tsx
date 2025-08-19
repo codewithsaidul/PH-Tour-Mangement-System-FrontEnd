ae0cf4f6989b5f94e0d7304c39b900ba8e1d64e2
codewithsaidul <codewithsaidul@gmail.com>
2025-08-19 22:25:41 +0600

    feat: date picker added on tour form

--- src/pages/admin/AddTour.tsx
+++ src/pages/admin/AddTour.tsx
@@ -1,9 +1,325 @@
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
+import { useGetAllDivisionQuery } from "@/redux/feature/division/division.api";
+import { useGetTourTypesQuery } from "@/redux/feature/tour/tour.api";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { format, formatISO } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import z from "zod";
 
+const tourSchema = z.object({
+  title: z
+    .string()
+    .nonempty("Title is required")
+    .min(3, "title must be contain at least 3 characters minimum"),
+  description: z
+    .string()
+    .nonempty("Description is required")
+    .min(20, "Description must be contain at least 20 characters minimum"),
+  startDate: z.date({ message: "Start Date is required" }),
+  endDate: z.date({ message: "End Date is required" }),
+  division: z.string().min(1, "Division is required"),
+  tourType: z.string().min(1, "Tour Types is requird"),
+});
 
 const AddTour = () => {
-  return (
-    <div>AddTour</div>
-  )
-}
+  const { data: divisionData, isLoading: divisionLoading } =
+    useGetAllDivisionQuery(undefined);
+  const { data: tourTypeData, isLoading: tourTypeLoading } =
+    useGetTourTypesQuery(undefined);
 
-export default AddTour
+  const form = useForm<z.infer<typeof tourSchema>>({
+    resolver: zodResolver(tourSchema),
+    defaultValues: {
+      title: "Cox's Bazar Beach Adventure",
+      description: "",
+      division: "",
+      tourType: "",
+    },
+  });
+
+  const divisionOptions = divisionData?.map(
+    (item: { _id: string; name: string }) => ({
+      value: item._id,
+      label: item.name,
+    })
+  );
+  const tourTypeOptions = tourTypeData?.data?.map(
+    (item: { _id: string; name: string }) => ({
+      value: item._id,
+      label: item.name,
+    })
+  );
+
+  const onSubmit = (values: z.infer<typeof tourSchema>) => {
+    const tourData = {
+      ...values,
+      startDate: formatISO(values.startDate),
+      endDate: formatISO(values.endDate),
+    };
+    console.log(tourData);
+  };
+
+  return (
+    <div className="w-full max-w-4xl mx-auto px-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>Add Tour</CardTitle>
+          <CardDescription>Add a new tour to the system</CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <Form {...form}>
+            <form
+              id="add-tour-form"
+              className="space-y-5"
+              onSubmit={form.handleSubmit(onSubmit)}
+            >
+              {/* ============ tour title ============ */}
+              <div>
+                <FormField
+                  control={form.control}
+                  name="title"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Tour Title</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              {/* ============= division & tour type =============== */}
+              <div className="flex flex-col min-[500px]:flex-row gap-5">
+                <FormField
+                  control={form.control}
+                  name="division"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Division</FormLabel>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                        disabled={divisionLoading}
+                      >
+                        <FormControl>
+                          <SelectTrigger className="w-full">
+                            <SelectValue />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          {divisionOptions?.map(
+                            (option: { value: string; label: string }) => (
+                              <SelectItem
+                                key={option.value}
+                                value={option.value}
+                              >
+                                {option.label}
+                              </SelectItem>
+                            )
+                          )}
+                        </SelectContent>
+                      </Select>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="tourType"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Tour Type</FormLabel>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                        disabled={tourTypeLoading}
+                      >
+                        <FormControl>
+                          <SelectTrigger className="w-full">
+                            <SelectValue />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          <SelectContent>
+                            {tourTypeOptions?.map(
+                              (option: { value: string; label: string }) => (
+                                <SelectItem
+                                  key={option.value}
+                                  value={option.value}
+                                >
+                                  {option.label}
+                                </SelectItem>
+                              )
+                            )}
+                          </SelectContent>
+                        </SelectContent>
+                      </Select>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              {/* ============= start & ending date =============== */}
+              <div className="flex flex-col min-[500px]:flex-row gap-5">
+                <FormField
+                  control={form.control}
+                  name="startDate"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col flex-1">
+                      <FormLabel>Start Date</FormLabel>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <FormControl>
+                            <Button
+                              variant={"outline"}
+                              className={cn(
+                                "w-full pl-3 text-left font-normal",
+                                !field.value && "text-muted-foreground"
+                              )}
+                            >
+                              {field.value ? (
+                                format(field.value, "PPP")
+                              ) : (
+                                <span>Pick a date</span>
+                              )}
+                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                            </Button>
+                          </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-auto p-0" align="start">
+                          <Calendar
+                            mode="single"
+                            selected={field.value}
+                            onSelect={field.onChange}
+                            disabled={(date) =>
+                              date <
+                              new Date(
+                                new Date().setDate(new Date().getDate() - 1)
+                              )
+                            }
+                            captionLayout="dropdown"
+                          />
+                        </PopoverContent>
+                      </Popover>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="endDate"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col flex-1">
+                      <FormLabel>End Date</FormLabel>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <FormControl>
+                            <Button
+                              variant={"outline"}
+                              className={cn(
+                                "w-full pl-3 text-left font-normal",
+                                !field.value && "text-muted-foreground"
+                              )}
+                            >
+                              {field.value ? (
+                                format(field.value, "PPP")
+                              ) : (
+                                <span>Pick a date</span>
+                              )}
+                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                            </Button>
+                          </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-auto p-0" align="start">
+                          <Calendar
+                            mode="single"
+                            selected={field.value}
+                            onSelect={field.onChange}
+                            disabled={(date) =>
+                              date <
+                              new Date(
+                                new Date().setDate(new Date().getDate() - 1)
+                              )
+                            }
+                            captionLayout="dropdown"
+                          />
+                        </PopoverContent>
+                      </Popover>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              {/* ========== tour description ======================= */}
+              <div>
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Textarea
+                          {...field}
+                          className="resize-none h-40 no-scrollbar"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+
+        <CardFooter className="flex justify-end">
+          <Button type="submit" form="add-tour-form" className="cursor-pointer">
+            Create Tour
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default AddTour;
